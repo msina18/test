5590ec6ea22ef15ca0b1a12eb294ca0a70b72622
Mohamadsina85 <155167077+Mohamadsina85@users.noreply.github.com>
2024-01-02 14:00:30 +0330

    counting vowels

---
+++ شمارش حروف صدا دار.py
@@ -0,0 +1,27 @@
+def v (str):
+    n = {'a', 'e','i','o','u'}
+    c = 0
+    r = []
+    for i in str :
+        if i.lower() in n:
+            r.append(i)
+            c+= 1 
+    return c, r
+        
+print(v(input('enter a name :')))  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
